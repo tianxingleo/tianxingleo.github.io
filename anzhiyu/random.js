ebf563fed7077e7f521d5d464de5a8c0277ae2b2
tianxingleo <tianxingleo@gmail.com>
2025-10-15 16:49:19 +0800

    Site updated: 2025-10-15 16:49:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/14/hello-world/"];function toRandomPost(){
+var posts=["2025/10/14/hello-world/","2025/10/15/我的新博客的第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
