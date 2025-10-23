72ff942f342cf458f1a88f92e0c23a77865c32d7
tianxingleo <tianxingleo@gmail.com>
2025-10-23 20:36:05 +0800

    Site updated: 2025-10-23 20:36:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/15/我的新博客的第一篇文章/","2025/10/22/关于回文质数筛选的优化路径/"];function toRandomPost(){
+var posts=["2025/10/15/我的新博客的第一篇文章/","2025/10/22/关于回文质数筛选的优化路径/","2025/10/23/质数筛序算法的汇总/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
